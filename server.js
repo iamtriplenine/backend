822b280aaaef50383e4a50c2398f71ecbf4f5f3f
iamtriplenine <iamivanntresor999@gmail.com>
2026-01-05 20:46:47 +0000

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,6 @@
     const express = require('express');
 const cors = require('cors');
 const { Pool } = require('pg');
-const cron = require('node-cron');
 const app = express();
 
 app.use(cors());
